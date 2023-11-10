55778c4aba65bb6ef5db03f3dc9fcb7271e1dfe4
t-fowler <52936210+t-fowler@users.noreply.github.com>
2023-11-09 22:16:32 -0800

    Initial commit

---
+++ ch1-tutorial.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+void printHalfSquare() {
+    for (int row = 0; row < 5; row++) {
+        for (int numHashes = 5; numHashes > row; numHashes--) {
+            cout << '#';
+        }
+        cout << '\n';
+    }
+}
+
+void printTriangle() {
+    for (int row = 1; row < 8; row++) {
+        for (int numHashes = 1; numHashes <= 4 - abs(4 - row); numHashes++) {
+            cout << '#';
+        }
+        cout << '\n';
+    }
+}
+
+int main() {
+    printHalfSquare();
+    cout << '\n';
+    printTriangle();
+}
