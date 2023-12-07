cf9808941748652d7e345fa5b0329e37a8a8cea0
Tyler Fowler <52936210+t-fowler@users.noreply.github.com>
2023-12-06 22:37:18 -0800

    Implemented Luhn's checksum validation.

--- ch1-tutorial.cpp
+++ ch1-tutorial.cpp
@@ -21,9 +21,53 @@ void printTriangle() {
     }
 }
 
+// Input must be single digit number.
+int doubleAndSumDigits(char digit) {
+    int result = (digit - '0') * 2;
+    if (result >= 10) {
+        result -= 9;
+    }
+    return result;
+}
+
+/*
+ * Reads an integer from stdin and validates the Luhn checksum. Checksum is valid
+ * when the sum of all digits is a multiple of 10. Every other digit, starting
+ * second from the right, is doubled and the digits of the doubled number are summed separately.
+ */
+void luhnsChecksum() {
+    char digit;
+    int evenSum = 0, oddSum = 0, position = 1;
+
+    std::cin.get(digit);
+    while(digit != '\n') {
+        if (position % 2 == 0) {
+            oddSum += doubleAndSumDigits(digit);
+            evenSum += digit - '0';
+        } else {
+            oddSum += digit - '0';
+            evenSum += doubleAndSumDigits(digit);
+        }
+        std::cin.get(digit);
+        position++;
+    }
+    
+    int checksum;
+    if ((position - 1) % 2 == 0) checksum = evenSum;
+    else checksum = oddSum;
+    if (checksum % 10 == 0) {
+        std::cout << "Checksum is divisible by 10. Valid." << '\n';
+    } else {
+        std::cout << "Checksum is not divisible by 10. Invalid." << '\n';
+    }
+}
+
 // Testing Git
 int main() {
+    /*
     printHalfSquare();
     cout << '\n';
     printTriangle();
+    */
+    luhnsChecksum();
 }
