81d563825d97bee97ac92dad9581a60cfffa6d99
Tyler Fowler <52936210+t-fowler@users.noreply.github.com>
2024-01-22 23:06:09 -0800

    Created a base conversion program that can convert integers of any base <= 35 to any other base <= 35. BaseConversion.cpp is a solution to exercises 2-6, 2-7, and 2-9.

--- ch1/BaseConversion.cpp
+++ ch1/BaseConversion.cpp
@@ -1,11 +1,10 @@
+#include <cmath>
 #include <iostream>
 #include <string>
 
 namespace TLAP {
 
-using std::cin;
-using std::cout;
-using std::string;
+using namespace std;
 
 int charToDigit(char c) {
     if (c >= '0' && c <= '9') {
@@ -25,12 +24,34 @@ int charToDigit(char c) {
     }
 }
 
-int baseNToDecimal(string integer, int n) {
+char digitToChar(int digit) {
+    if (digit > 9) {
+        return digit + '0' + 7;
+    }
+
+    return digit + '0';
+}
+
+int baseNToDecimal(string integerString, int n) {
     int decimal = 0;
-    for (auto digit : integer) {
+    for (auto digit : integerString) {
         decimal = n * decimal + charToDigit(digit);
     }
     return decimal;
+}
+
+string decimalToBaseN(int decimal, int n) {
+    string integerString = "";
+    // divide the largest power of n into decimal
+    // grab the remainder to continue the process.
+    do {
+        int largestPower = pow(n, floor(log(decimal) / log(n)));
+        int digit = decimal / largestPower;
+        integerString += digitToChar(digit);
+        decimal = decimal % largestPower;
+    } while (decimal >= n);
+
+    return integerString;
 }
 
 string changeBase(string integer, int originalBase, int newBase) {
@@ -42,14 +63,13 @@ string changeBase(string integer, int originalBase, int newBase) {
 
 } // namespace TLAP
 
-/*
- * 
- */
+
 int main() {
     std::string integerString;
     int base = 0;
+    int decimal;
 
-    std::cout << "Input a binary number:\n";
+    std::cout << "Input an integer in any base <= 35:\n";
     std::cin >> integerString;
     std::cout << "What is the base?\n";
     std::cin >> base;
@@ -60,6 +80,13 @@ int main() {
     }
 
     std::cout << TLAP::baseNToDecimal(integerString, base) << '\n';
+
+    std::cout << "Input a decimal integer:\n";
+    std::cin >> decimal;
+    std::cout << "What base would you like to convert to?\n";
+    std::cin >> base;
+
+    std::cout << TLAP::decimalToBaseN(decimal, base) << std::endl;
 
     return 0;
 }
