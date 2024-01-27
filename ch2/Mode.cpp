128f6777d8db5ecc1cf80a2e4150013464f2c2f7
Tyler Fowler <52936210+t-fowler@users.noreply.github.com>
2024-01-26 22:23:19 -0800

    Added enclosing namespace.

--- ch2/Mode.cpp
+++ ch2/Mode.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <map>
 
+namespace TLAP {
+
+// 3-7
 // Pre-condition: size > 0
 int mode(int arr[], size_t size) {
     std::map<int, int> histogram;
@@ -20,16 +23,18 @@ int mode(int arr[], size_t size) {
     return most_frequent;
 }
 
+} // namespace TLAP
+
 int main() {
     int test1[10] = {0, 1, 5, 5, 1, 3, 5, 2, 0, 10,};
     int test2[10] = {0, 1, 675, 675, 1, 3, 675, 2, 0, 10,};
     int test3[1] = {999};
     int test4[5] = {1, 1, 2, 2, 10};
     
-    assert(mode(test1, 10) == 5);
-    assert(mode(test2, 10) == 675);
-    assert(mode(test3, 1) == 999);
-    assert(mode(test4, 5) == 1);
+    assert(TLAP::mode(test1, 10) == 5);
+    assert(TLAP::mode(test2, 10) == 675);
+    assert(TLAP::mode(test3, 1) == 999);
+    assert(TLAP::mode(test4, 5) == 1);
 
     std::cout << "Passed all tests.\n";
 }
